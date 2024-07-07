251f0e1301a38d9d8b482a6c6d957b05628a1664
Ihor Sokolov <i.sokolov@geniusee.com>
2024-07-07 10:42:31 +0300

    add kwargs

--- auction/user/urls.py
+++ auction/user/urls.py
@@ -6,7 +6,7 @@ app_name = "user"
 
 urlpatterns = [
     path(route="register/", view=RegisterUserView.as_view(), name="register"),
-    path("login/", view=TokenObtainPairView.as_view(), name="login"),
-    path("refresh/", view=TokenRefreshView.as_view(), name="refresh"),
-    path("revoke/", view=TokenBlacklistView.as_view(), name="revoke"),
+    path(route="login/", view=TokenObtainPairView.as_view(), name="login"),
+    path(route="refresh/", view=TokenRefreshView.as_view(), name="refresh"),
+    path(route="revoke/", view=TokenBlacklistView.as_view(), name="revoke"),
 ]
