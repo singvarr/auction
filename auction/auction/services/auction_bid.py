731f6ba6c478e8d03698f4ff7f9497156efc549b
Ihor Sokolov <i.sokolov@geniusee.com>
2024-07-17 19:35:51 +0300

    add business logic for bids

---
+++ auction/auction/services/auction_bid.py
@@ -0,0 +1,38 @@
+from typing import Optional
+from auction.auction.models import Auction, AuctionBid
+from auction.auction.exceptions import InvalidAuctionStatusException, InvalidBidValueException
+from auction.user.models import User
+from auction.notification.service import NotificationService
+
+
+class AuctionBidService:
+    def __init__(self) -> None:
+        self._notification_service = NotificationService()
+
+    def find_latest_bid(self, auction: Auction) -> Optional[AuctionBid]:
+        return (
+            AuctionBid.objects
+            .filter(auction_id=auction.pk)
+            .order_by("-created_at")
+            .first()
+        )
+
+    def create(self, data, user: User, auction: Auction) -> AuctionBid:
+        if auction.status != Auction.Status.ACTIVE:
+            raise InvalidAuctionStatusException()
+
+        value = data["value"]
+
+        latest_bid = self.find_latest_bid(auction=auction)
+
+        if (
+            (not latest_bid and value <= auction.lot.initial_price) or
+            (latest_bid and value <= latest_bid.value)
+        ):
+            raise InvalidBidValueException()
+
+        new_bid = AuctionBid.objects.create(value=value, made_by=user, auction=auction)
+
+        self._notification_service.send_notification_about_new_bid(bid=new_bid, socket_id=data["socket_id"])
+
+        return new_bid
--- auction/auction/services/manage_status_auction.py
+++ auction/auction/services/manage_status_auction.py
@@ -1,13 +1,16 @@
 from datetime import datetime
+from django.db import transaction
 from auction.auction.models import Auction
 from auction.auction.exceptions import InvalidAuctionStatusException
 from auction.notification.service import NotificationService
+from auction.auction.services.auction_bid import AuctionBidService
 
 
 class ManageStatusAuctionService:
     def __init__(self, auction: Auction) -> None:
         self._auction = auction
         self._notification_service = NotificationService()
+        self._auction_bid_service = AuctionBidService()
 
     def start(self) -> Auction:
         if self._auction.status != Auction.Status.NOT_CONDUCTED:
@@ -25,10 +28,20 @@ class ManageStatusAuctionService:
         if self._auction.status != Auction.Status.ACTIVE:
             raise InvalidAuctionStatusException(detail=f"Cannot finish an auction with status {self._auction.status}")
 
-        self._auction.finished_at = datetime.now()
-        self._auction.status = Auction.Status.FINISHED
-        self._auction.save()
+        latest_bid = self._auction_bid_service.find_latest_bid(auction=self._auction)
 
-        self._notification_service.send_notification_about_finished_auction(auction=self._auction)
+        with transaction.atomic():
+            self._auction.finished_at = datetime.now()
+            self._auction.status = Auction.Status.FINISHED
 
-        return self._auction
+            if latest_bid:
+                self._auction.winner = latest_bid.made_by
+
+                self._auction.lot.sold_price = latest_bid.value
+                self._auction.lot.save()
+
+            self._auction.save()
+
+            self._notification_service.send_notification_about_finished_auction(auction=self._auction)
+
+            return self._auction
