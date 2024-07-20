ca9243d879517e5d0bd0e4f1fbe4769c1981c7f5
Ihor Sokolov <i.sokolov@geniusee.com>
2024-07-20 16:08:38 +0300

    install

--- auction/urls.py
+++ auction/urls.py
@@ -18,12 +18,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("auctions/", include("auction.auction.urls")),
     path("users/", include("auction.user.urls")),
     path("notifications/", include("auction.notification.urls")),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
 
 if settings.DEBUG:
