000ab3aa333917df25ff9596d580f30b79bd54bd
Ihor Sokolov <sihorjs@gmail.com>
2024-09-20 13:46:14 +0300

    retrieve user API (#13)

--- auction/user/views.py
+++ auction/user/views.py
@@ -1,10 +1,17 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, RetrieveAPIView
 from rest_framework.parsers import MultiPartParser
-from rest_framework.permissions import AllowAny
-from auction.user.serializers import RegisterUserSerializer
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from auction.user.serializers import RegisterUserSerializer, RetrieveUserSerializer
+from auction.user.models import User
 
 
 class RegisterUserView(CreateAPIView):
     permission_classes = (AllowAny,)
     serializer_class = RegisterUserSerializer
     parser_classes = [MultiPartParser]
+
+
+class RetrieveUserView(RetrieveAPIView):
+    queryset = User.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = RetrieveUserSerializer
